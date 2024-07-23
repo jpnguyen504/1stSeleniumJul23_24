a0886cc12272c01e296526a248bd86cd524da780
jpnguyen504 <jpnguyen504@gmail.com>
2024-07-23 14:54:37 -0500

    second feature

---
+++ src/launchBrowser/GitTest2.java
@@ -0,0 +1,15 @@
+package launchBrowser;
+
+public class GitTest2 {
+	public static void main(String[] args) {
+		System.out.println("second branch");
+		printMe();
+	}
+	
+	public static void printMe() {
+		System.out.println("git test 1");
+		System.out.println("git test 2");
+		System.out.println("git test 3");
+		System.out.println("git test 4");
+	}
+}
